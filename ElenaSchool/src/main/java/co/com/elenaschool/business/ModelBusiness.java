a525ca4a1a68e637a3c252058567cfa1a2817a5f
AdrianL <AdrianL@AdrianL.DigitalWare.com.co>
2016-09-20 16:41:16 -0500

    Implementación servicio obtener estructura tablas

---
+++ ElenaSchool/src/main/java/co/com/elenaschool/business/ModelBusiness.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.elenaschool.business;
+
+import co.com.elenaschooldataaccess.persistencia.contract.IModelDao;
+import co.com.elenaschooldataaccess.persistencia.dataaccess.ModelDao;
+import co.com.elenaschooldataaccess.persistencia.model.Model;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+/**
+ * Clase que maneja la lógica de los formularios dinamicos de la APP
+ * @created 20-sep-2016 08:38:26
+ * @author AdrianL
+ */
+@Service
+public class ModelBusiness {
+    
+    private IModelDao iModelDao;
+    
+    public ModelBusiness(){
+        iModelDao = new ModelDao();
+    }
+    
+    /**
+     * Obtiene la estructura de una tabla
+     * @return 
+     */
+    public List<Model> getEstructuraTabla(){
+        return iModelDao.readModel(new Model());
+    }
+}
--- ElenaSchool/src/main/java/co/com/elenaschool/service/Service.java
+++ ElenaSchool/src/main/java/co/com/elenaschool/service/Service.java
@@ -6,6 +6,9 @@
 package co.com.elenaschool.service;
 
 import co.com.elenaschool.business.LoginBusiness;
+import co.com.elenaschool.business.ModelBusiness;
+import co.com.elenaschooldataaccess.persistencia.model.Model;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,10 +27,19 @@ public class Service {
     @Autowired
     LoginBusiness loginBusiness;
     
+    @Autowired
+    ModelBusiness modelBusiness;
+    
     @CrossOrigin(origins = "*")
     @RequestMapping(value = "/web/elenaschool/login", method = RequestMethod.GET, produces = "application/json")
     public String login(){
         return loginBusiness.generateToken();
+    }
+    
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/web/elenaschool/getEstructuraTabla", method = RequestMethod.GET, produces = "application/json")
+    public List<Model> getEstructuraTabla(){
+        return modelBusiness.getEstructuraTabla();
     }
     
     
