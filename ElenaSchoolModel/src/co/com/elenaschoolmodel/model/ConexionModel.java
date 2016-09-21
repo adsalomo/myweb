d1ede321435af6cb3d02636ecbed7e149b822214
AdrianL <AdrianL@AdrianL.DigitalWare.com.co>
2016-09-21 11:17:19 -0500

    Configuración de capas

---
+++ ElenaSchoolModel/src/co/com/elenaschoolmodel/model/ConexionModel.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.elenaschoolmodel.model;
+
+/**
+ * Clase propiedades conexión
+ * @author AdrianL
+ */
+public class ConexionModel {
+    private String driverClassName;
+    private String url;
+    private String userName;
+    private String password;
+
+    /**
+     * @return the driverClassName
+     */
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    /**
+     * @param driverClassName the driverClassName to set
+     */
+    public void setDriverClassName(String driverClassName) {
+        this.driverClassName = driverClassName;
+    }
+
+    /**
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @param url the url to set
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
---
+++ ElenaSchoolModel/src/co/com/elenaschoolmodel/model/GrupoAcademicoModel.java
@@ -0,0 +1,12 @@
+package co.com.elenaschoolmodel.model;
+
+
+/**
+ * @author AdrianL
+ * @version 1.0
+ * @created 17-ago-2016 08:42:11
+ */
+public class GrupoAcademicoModel {
+
+	
+}//end GrupoAcademicoModel
