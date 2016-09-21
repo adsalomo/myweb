be1160d6519d062d6cbd569117841cdddaa511d6
AdrianL <AdrianL@AdrianL.DigitalWare.com.co>
2016-09-21 16:33:31 -0500

    Implementación View

--- ElenaSchoolClient/public_html/scripts/app/controllers/mainController.js
+++ ElenaSchoolClient/public_html/scripts/app/controllers/mainController.js
@@ -3,5 +3,31 @@
  * @since 21 sep 2016
  * @description controller principal
  */
+app.controller('mainController', ['$scope', '$http', '$myService',
+    function ($scope, $http, $myService) {
+        
+        /**
+         * Definicion de variables
+         */
+        $scope.modelEstructura = [];
+        
+        /**
+         * Obtiene la estructura de una tabla de la base de datos
+         * @param {type} nameTable Nombre tabla a obtener estructura
+         * @returns {undefined}
+         */
+        $scope.getEstructuraTablaAction = function (nameTable) {
+
+            $myService.getEstructuraTablaService(nameTable)
+                    .success(function (data, status, headers, config) {
+
+
+
+                    }).error(function (data, status, headers, config) {
+                console.log(data);
+            });
+        };
+
+    }]);
 
 
--- ElenaSchoolClient/public_html/scripts/app/services/service.js
+++ ElenaSchoolClient/public_html/scripts/app/services/service.js
@@ -3,5 +3,21 @@
  * @since 21 sep 2016
  * @description request a los servicios
  */
+app.factory('$myService', ['$http', function ($http) {
+    return {
+        
+        getEstructuraTablaService: function (nameTable) {
+            return $http({
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json; charset=utf-8'
+                },
+                url: 'http://localhost:8081/web/elenaschool/getEstructuraTabla',
+                data: { nameTable: nameTable }
+            });
+        }
+        
+    }
+}]);
 
 
